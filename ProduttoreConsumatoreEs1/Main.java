45606bdd574e6a1705897f7cb17b8b598815b4ce
davideIsEnough <davide.silano1406@gmail.com>
2024-10-03 20:38:57 +0200

    Main.java

--- ProduttoreConsumatoreEs1/Main.java
+++ ProduttoreConsumatoreEs1/Main.java
@@ -5,26 +5,21 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        // Chiedere all'utente di inserire la dimensione del buffer T
         System.out.print("Inserisci la dimensione del buffer: ");
         int N = scanner.nextInt();
-
-        // Chiedere all'utente di inserire il numero di produttori e consumatori N
+        //T non viene usata verranno creati solo un thread consumatore e uno produttore
         System.out.print("Inserisci il numero di produttori e consumatori: ");
         int T = scanner.nextInt();
 
         // Creare il buffer con dimensione T
         Buffer buffer = new Buffer(N);
 
-        // Creare e avviare N produttori e N consumatori
-        for (int i = 0; i < T; i++) {
-            Produttore produttore = new Produttore(buffer);
-            Consumatore consumatore = new Consumatore(buffer);
+        Produttore produttore = new Produttore(buffer);
+        Consumatore consumatore = new Consumatore(buffer);
 
-            // Avviare i thread del produttore e del consumatore
-            new Thread(produttore).start();
-            new Thread(consumatore).start();
-        }
+        new Thread(produttore).start();
+        new Thread(consumatore).start();
+
         scanner.close();
     }
 }
